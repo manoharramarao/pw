a0861b65f4e97a87dd81bcb2f00f27de393f9e2c
msr <manoharramarao@gmail.com>
2023-03-04 22:37:55 +0530

    fix2

--- push.js
+++ push.js
@@ -1,18 +1,11 @@
 var push = require('web-push');
 
-let vapidKeys = push.generateVAPIDKeys();
-console.log(vapidKeys);
+// let vapidKeys = push.generateVAPIDKeys();
+// console.log(vapidKeys);
 
-push.setVapidDetails('mailto: test@code.co.in', vapidKeys.publicKey, vapidKeys.privateKey);
+push.setVapidDetails('mailto: test@code.co.in', 'BHDTngWIhVl2pm85P7KCAST59CvizykEjkk6XaVorjijsuGmAdDNr8524T1f7aQEYHHrYMD72RNuaESwv0FW2A4', 'Tspntl75iJZNbykOHbFJI2-GrgqEm5cCw_5BemFFCVQ');
 
-let sub = {
-  endpoint: "https://fcm.googleapis.com/fcm/send/fzJXBZkNl1Q:APA91bHMK2X1vScVDKWHFoIaVNBUCq6iysj0HOXulCjijjBRVF_oRRMq8O_RzzSCcXQLgwHOp5wTIMnkoqKFJBQRkg5YPStrcsD4JM0Tj15aqgOUmIr3Zo4AXW6vHHO5aJRTGu8807tA",
-  expirationTime: null,
-  keys: {
-    p256dh: "BCdPDhYZ8oa38Wq5PbnQSNkV-Et7f6PkuetjWr_3hJ0Ka1oVSS06XydD2qHVy_WCDv0B0-h5fTbeT3_CJXNGzGs",
-    auth: "HCUfvrkxHzN4Lt2OkXljTA"
-  }
-}
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fXWr0d6waUU:APA91bGwMcMl8R4Gz4xh1xRgZ2hvLVPhsAzJ18KM4gc56q-x6pm8L20vBGCzQcu2AZJJsZih4RXBet7QuWcrFpSbrNhU1K6VYODChFMAUeq8utBFPOHJgeG7NKdDWFz3MjEuhXdfgIJY","expirationTime":null,"keys":{"p256dh":"BBO82ZwN9DPoaySFgnALuCJiGgQbrJfM91GpTE_1bzzZ0zYd6Ob322kL7oQFlQf5I2L0kYJq7jMTN4xAn_-AJQk","auth":"0le9eoMlaeD5wKo2pZriHA"}}
 
 // let sub = {endpoint:"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABkA…CT4C3koYqvB9c-Yhc14lpEoZ8jRy41TAieC2xQ8QSu0LQYZIzFKAsIYmwteY",expirationTime:null,keys:{auth:"b1_8s9vqUYC8IIzAJ6s2Fw",p256dh:"BNJXiX0rtYJYr8vWIWB4Dt67JuVNm1TQP8DdCIRNX--uioAugNdthjatkylyrYIctyNrITXZ_OB9dHsy9NFCyZY"}}
 
--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 self.addEventListener('push', (e) => {
-  var options = {
-    body: 'This notification was generated from a push!',
-    icon: 'images/example.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: '2'
-    },
-    actions: [
-      { action: 'explore', title: 'Explore this new world', icon: 'images/checkmark.png' },
-      {action: 'close', title: 'Close', icon: 'images/xmark.png'},
-    ]
-  }
-  e.waitUntil(self.registration.sendNotification('test message', options));
+  // var options = {
+  //   body: 'This notification was generated from a push!',
+  //   icon: 'images/example.png',
+  //   vibrate: [100, 50, 100],
+  //   data: {
+  //     dateOfArrival: Date.now(),
+  //     primaryKey: '2'
+  //   },
+  //   actions: [
+  //     { action: 'explore', title: 'Explore this new world', icon: 'images/checkmark.png' },
+  //     {action: 'close', title: 'Close', icon: 'images/xmark.png'},
+  //   ]
+  // }
+  e.waitUntil(self.registration.sendNotification('test message', {}));
 });
