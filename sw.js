021334a655abbfeccaf0d13688c63e77630257f3
msr <manoharramarao@gmail.com>
2023-03-05 00:57:35 +0530

    fix8

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', (e) => {
   var options = {
-    body: 'This notification was generated from a push!',
+    body: JSON.stringify(e),
     icon: 'images/example.png',
     vibrate: [100, 50, 100],
     data: {
