86e33f58195c030cd128c1d42ad3345ad3e8647c
msr <manoharramarao@gmail.com>
2023-03-05 21:17:02 +0530

    pwa fix 10

--- sw.js
+++ sw.js
@@ -13,7 +13,11 @@ self.addEventListener("install", installEvent => {
       cache.addAll(assets)
     })
   )
-})
+});
+
+this.addEventListener('fetch', function (event) {
+    // it can be empty if you just want to get rid of that error
+});
 
 
 self.addEventListener('push', (e) => {
